0560c6cb16c21df463ceea8211bfeb2d46853d14
CloudSweet <cloud.sweet@yahoo.com>
2019-08-20 10:46:42 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SCloudAPI
 Stand-alone API for SCloud.
+
+## 当前版本
+### 0.1
+
+## 使用方法
+* 1.配置config.php
+* 2.访问测试
+* 3.填入插件后台
+
+## 版本历史
+* 0.1 项目部分代码引入，功能开发中
---
+++ SCloudAPI/config.php
@@ -0,0 +1,8 @@
+<?php
+$db_host = 'localhost';
+$db_port = 3306;
+$db_username = 'root';
+$db_password = 'password';
+$db_name = 'db_name';
+$cc_encryption_hash = 'cc_encryption_hash';
+$encryption_hash = 'Node12345678';
